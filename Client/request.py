bdcda5ce9b522cf6930301843d8a407ce2ac7504
mar99ta <56344087+mar99ta@users.noreply.github.com>
2021-05-24 10:51:31 +0200

    dodanie haszowania haseł

--- Client/request.py
+++ Client/request.py
@@ -1,4 +1,6 @@
 import json
+from cryptography.hazmat.primitives import hashes
+import hashlib
 
 class Request:
 
@@ -7,13 +9,16 @@ class Request:
 
     def logIn(self, login, password):
         request = self.request
+        password = self.make_hash(str.encode(password))
         request["signal"] ="LOG"
         request["data"] = {"login": login, "password": password}
+        print(str(request))
 
         return str(request)
 
     def register(self, login, password, auth_key):
         request = self.request
+        password = self.make_hash(str.encode(password))
         request["signal"] ="LAD"
         request["data"] = {"login": login, "password": password, "auth_key": auth_key}
 
@@ -26,3 +31,11 @@ class Request:
         request["data"] = {"to": to, "from": sender, "message": message}
 
         return str(request)
+
+
+    def make_hash(self, to_hash):
+        m = hashlib.sha512()
+        m.update(to_hash)
+        hash = m.hexdigest()
+
+        return hash
