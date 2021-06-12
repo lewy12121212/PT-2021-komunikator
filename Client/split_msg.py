403bda57b95dde72f974bad004d1b06d8bdc7d15
Lewy <lewy12121212@gmail.com>
2021-06-12 22:21:37 +0200

    Add split def

---
+++ Client/split_msg.py
@@ -0,0 +1,30 @@
+
+def split_by_chars(word):
+    return [char for char in word]
+
+def split_msg(msg):
+    iterator = 0
+
+    while iterator <= len(msg):
+        if iterator % 40 == 0:
+            if msg[iterator] == " ":
+                msg.insert(iterator, '\n')
+            else:
+                j = iterator
+                while True:
+                    if msg[j] == " ":
+                        msg.insert(j, '\n')
+                        break
+                    else:
+                        j -= 1
+            
+        iterator += 1
+
+    msg_join = ''.join(msg)
+    print(msg_join)
+    return msg_join
+
+def main_split(msg):
+    msg_chars = split_by_chars(msg)
+    msg_join = split_msg(msg_chars)
+    return msg_join
